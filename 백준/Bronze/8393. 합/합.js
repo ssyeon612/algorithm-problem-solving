988731f0f2b9e9a9efff2971241c60df34f161a9
정승연 Jeong Seung Yeon <ssyeon612@gmail.com>
2023-11-28 17:26:17 +0900

    [Bronze V] Title: 합, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = +fs.readFileSync('/dev/stdin').toString().trim();
+
+let result = 0;
+
+for(let i=1; i<=input; i++) {
+    result += i;
+}
+
+console.log(result);

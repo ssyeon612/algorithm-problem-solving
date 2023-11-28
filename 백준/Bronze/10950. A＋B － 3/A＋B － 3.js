d8dcaa0aab2fd0742cbf041fa71296605510191f
정승연 Jeong Seung Yeon <ssyeon612@gmail.com>
2023-11-28 17:20:30 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const [n, ...arr] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 for(let i=0; i<n; i++) {
-    const [a, b] = arr[i].split(' ').map(Number);
-    console.log(a+b);
+    const [a, b] = arr[i].split(' ');
+    console.log(parseInt(a) + parseInt(b));
 }

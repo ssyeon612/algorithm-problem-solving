ad8ac7980a1e5b8cfed35bd23441af26267b08d4
정승연 Jeong Seung Yeon <ssyeon612@gmail.com>
2023-11-29 12:13:00 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
-const input = +fs.readFileSync('/dev/stdin').toString().trim();
+const input = fs.readFileSync('/dev/stdin').toString().trim();
 
-let result = 0;
-
-for(let i=1; i<=input; i++) {
-    result += i;
+let total = 0;
+for (let i = 1; i <= input; i++) {
+    total += i;
 }
-
-console.log(result);
+console.log(total);

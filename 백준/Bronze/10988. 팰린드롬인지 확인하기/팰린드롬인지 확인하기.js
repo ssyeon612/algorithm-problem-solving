a9e55c72e9ab74e38d263ff31a5c6346735c8db8
정승연 Jeong Seung Yeon <ssyeon612@gmail.com>
2023-12-06 12:40:33 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const reverse = input.split("").reverse().join("");
+console.log(input === reverse ? 1 : 0);

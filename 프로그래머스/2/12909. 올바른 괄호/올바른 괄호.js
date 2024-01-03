45564e7436673114cf8b8f919bbd35bff0a01472
정승연 Jeong Seung Yeon <ssyeon612@gmail.com>
2024-01-03 15:29:24 +0900

    [level 2] Title: 올바른 괄호, Time: 3.02 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s){
+    let count = 0;
+    
+    for(let i=0; i<s.length; i++) {
+        if (s[i] === '(') {
+            count++;
+        } else if(s[i] === ')') {
+            count--;
+            if(count < 0) {
+                return false
+            }
+        }
+    }
+    
+    return count === 0;
+}
